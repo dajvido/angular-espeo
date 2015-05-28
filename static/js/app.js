f39de250af162385c7337e5829d89d8bd1f08a42
dajvido <dawid.jaskot@gmail.com>
2015-05-28 20:39:12 +0200

    Add service

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 (function () {
     angular.module('avengers', [
-        'ngRoute'
+        'ngRoute',
+        'avengers.service'
     ])
         .config(function($routeProvider) {
             $routeProvider
@@ -22,10 +23,10 @@
         .controller('AvengerCtrl', AvengerCtrl)
         .filter('checkmark', CheckMarkFilter);
 
-    function AvengersListCtrl() {
+    function AvengersListCtrl(AvengersRepository) {
         var ths = this;
         ths.ultimateAnswer = 42;
-        ths.avengers = listOfAvengers;
+        ths.avengers = AvengersRepository.fetchAll();
         ths.newAvenger = {
             hasOwnSuperPowers: false
         };
@@ -37,9 +38,9 @@
         }
     }
 
-    function AvengerCtrl($routeParams) {
+    function AvengerCtrl($routeParams, AvengersRepository) {
         var ths = this;
-        ths.avenger = listOfAvengers[$routeParams.avenger];
+        ths.avenger = AvengersRepository.fetchOne([$routeParams.avenger]);
     }
 
     function CheckMarkFilter () {
@@ -48,52 +49,4 @@
         }
         return filter;
     }
-
-    var listOfAvengers = [
-        {
-            'name': 'Iron Man',
-            'realName': 'Anthony Edward "Tony" Stark',
-            'species': 'human',
-            'origin': 'Earth',
-            'hasOwnSuperPowers': false,
-            'abilities': [
-                'Genius-level intellect',
-                'Highly proficient scientist, engineer, and businessperson',
-                'Has powered armored suit'
-            ],
-            'firstApperance': 1963,
-            'createdBy': ['Stan Lee', 'Larry Lieber', 'Don Heck', 'Jack Kirby'],
-            'avatarURL': 'http://upload.wikimedia.org/wikipedia/en/e/e0/Iron_Man_bleeding_edge.jpg'
-        },
-        {
-            'name': 'Hulk',
-            'realName': 'Robert Bruce Banner',
-            'species': 'human mutant',
-            'origin': 'Earth',
-            'hasOwnSuperPowers': true,
-            'abilities': [
-                'Hulk\'s Smash',
-                'Invulnerability',
-                'Superhuman strength, stamina, durability, regeneration, speed and endurance'
-            ],
-            'firstApperance': 1962,
-            'createdBy': ['Stan Lee', 'Jack Kirby'],
-            'avatarURL': 'http://upload.wikimedia.org/wikipedia/en/3/3e/Incredible-hulk-20060221015639117.jpg'
-        },
-        {
-            'name': 'Thor',
-            'realName': 'Thor Odinson',
-            'species': 'Asgardian',
-            'origin': 'Asgard',
-            'hasOwnSuperPowers': true,
-            'abilities': [
-                'Superhuman strength, endurance, and longevity',
-                'Has Mjolnir'
-            ],
-            'firstApperance': 1962,
-            'createdBy': ['Stan Lee', 'Larry Lieber', 'Jack Kirby'],
-            'avatarURL': 'http://upload.wikimedia.org/wikipedia/en/4/41/Thor-272.jpg'
-        }
-    ];
-
 }());
---
+++ static/js/app.service.js
@@ -0,0 +1,62 @@
+(function () {
+   angular.module('avengers.service', [])
+       .service('AvengersRepository', AvengersRepository);
+
+    function AvengersRepository() {
+        return {
+            fetchAll: function() {
+                return listOfAvengers;
+            },
+            fetchOne: function(index) {
+                return listOfAvengers[index];
+            }
+        }
+    }
+
+    var listOfAvengers = [
+        {
+            'name': 'Iron Man',
+            'realName': 'Anthony Edward "Tony" Stark',
+            'species': 'human',
+            'origin': 'Earth',
+            'hasOwnSuperPowers': false,
+            'abilities': [
+                'Genius-level intellect',
+                'Highly proficient scientist, engineer, and businessperson',
+                'Has powered armored suit'
+            ],
+            'firstApperance': 1963,
+            'createdBy': ['Stan Lee', 'Larry Lieber', 'Don Heck', 'Jack Kirby'],
+            'avatarURL': 'http://upload.wikimedia.org/wikipedia/en/e/e0/Iron_Man_bleeding_edge.jpg'
+        },
+        {
+            'name': 'Hulk',
+            'realName': 'Robert Bruce Banner',
+            'species': 'human mutant',
+            'origin': 'Earth',
+            'hasOwnSuperPowers': true,
+            'abilities': [
+                'Hulk\'s Smash',
+                'Invulnerability',
+                'Superhuman strength, stamina, durability, regeneration, speed and endurance'
+            ],
+            'firstApperance': 1962,
+            'createdBy': ['Stan Lee', 'Jack Kirby'],
+            'avatarURL': 'http://upload.wikimedia.org/wikipedia/en/3/3e/Incredible-hulk-20060221015639117.jpg'
+        },
+        {
+            'name': 'Thor',
+            'realName': 'Thor Odinson',
+            'species': 'Asgardian',
+            'origin': 'Asgard',
+            'hasOwnSuperPowers': true,
+            'abilities': [
+                'Superhuman strength, endurance, and longevity',
+                'Has Mjolnir'
+            ],
+            'firstApperance': 1962,
+            'createdBy': ['Stan Lee', 'Larry Lieber', 'Jack Kirby'],
+            'avatarURL': 'http://upload.wikimedia.org/wikipedia/en/4/41/Thor-272.jpg'
+        }
+    ];
+}());
